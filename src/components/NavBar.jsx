e30a9a3f89b6cdedaad2a419ab6e7da6bd637669
jencba <jdamian@elam.co.uk>
2024-12-31 13:13:51 +0000

    styling changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 export const NavBar = () => {
-    return (
-        <nav className="navbar">
-     <div className="navbar-center">
-    <ul className="nav-links">
-    <li>
-        <Link to="/home">Home</Link>
-      </li>
-      <li>
-        <Link to="/articles">Articles</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-      <li>
-        <Link to="/post">Post</Link>
-      </li>
-      <li>
-        <Link to="/login">Log In</Link>
-      </li>
-    </ul>
-  </div>
-      </nav>
-    );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
+
+  return (
+    <nav className="navbar">
+      <button className="navbar-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <div className={`navbar-center ${menuOpen ? "show" : ""}`}>
+        <ul className="nav-links">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+          <li>
+            <Link to="/post">Post</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+        <select className="drop-down-menu" onChange={(e) => (window.location.href = e.target.value)}>
+          <option value="" selected="selected">
+            Menu
+          </option>
+          <option value="/home">Home</option>
+          <option value="/articles">Articles</option>
+          <option value="/topics">Topics</option>
+          <option value="/post">Post</option>
+          <option value="/login">Log In</option>
+        </select>
+      </div>
+    </nav>
+  );
+};
